d4e5b65e65659968841110973d05bba38fa6d0e6
Peter Popov <peter.popov@trading212.com>
2023-04-26 23:17:55 +0300

    Added logic for getting currency rates from BNB

---
+++ exchange_rates.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BNB_URL: str = "https://www.bnb.bg/Statistics/StExternalSector/StExchangeRates/StERForeignCurrencies/index.htm"
+
+cache = {}
+
+
+def get_currency_exchange_rate(currency_symbol: str, year, month, day):
+    key = f"{year}-{month}-{day}"
+
+    if key in cache:
+        return cache[key][currency_symbol]
+
+    html = requests.get(
+        BNB_URL, params={
+            "group1": "first",
+            "firstDays": day,
+            "firstMonths": month,
+            "firstYear": year
+        }
+    ).text
+    soup = BeautifulSoup(html, 'html.parser')
+    tags: list[Tag] = soup.select('table.table tbody tr:not(.last)')
+    names = ['name', 'symbol', 'quantity', 'rate', 'reverse_rate']
+    rates = []
+    for rows in tags:
+        row_data = []
+        for data in rows.select("td"):
+            row_data.append(data.text)
+        rates.append(dict(zip(names, row_data)))
+
+    rates = {rate['symbol']: rate for rate in rates}
+    rates['EUR'] = dict(
+        zip(
+            names,
+            ['Euro', 'EUR', '1', '1.95583', '0.5100']
+        )
+    )
+
+    cache[key] = rates
+
+    return rates.get(currency_symbol)
